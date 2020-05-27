5ac47bd8c1afb1649a5f6c90ca4b5c3e8998d8bf
chaolun <chaolun.w@redrabbitanimation.cn>
2020-05-27 14:14:30 +0800

    - Perfect C# get/set feature

--- Assets/Lua/Base.lua
+++ Assets/Lua/Base.lua
@@ -1,19 +1,22 @@
 Base = class()
 
 local function findAllProperties(t)
-    local props = {}
+    local get_props = {}
+    local set_props = {}
     for k, v in pairs(t) do
         if k == nil or v == nil then
         elseif "function" == type(v) and k:sub(1, 3) == "get" then
-            props[k:sub(4):gsub("^%u", string.lower)] = v
+            get_props[k:sub(4):gsub("^%u", string.lower)] = v
+        elseif "function" == type(v) and k:sub(1, 3) == "set" then
+            set_props[k:sub(4):gsub("^%u", string.lower)] = v
         end
     end
-    return props
+    return get_props, set_props
 end
 
 function Base:init()
     local t = getmetatable(self).__index
-    local props = findAllProperties(t)
+    local get_props, set_props = findAllProperties(t)
     setmetatable(
         self,
         {
@@ -22,10 +25,21 @@ function Base:init()
                     return t[k]
                 elseif "number" == type(k) and self.elements then
                     return self.elements[k]
-                elseif props[k] then
-                    return props[k](self)
+                elseif get_props[k] then
+                    return get_props[k](self)
                 else
                     return nil
+                end
+            end,
+            __newindex = function(self, k, v)
+                if get_props[k] then
+                    if set_props[k] then
+                        set_props[k](self, v)
+                    else
+                        Debugger.LogError("For the <b>" .. k .. "</b> property, you have implemented get" .. k:gsub("^%l", string.upper) .. "() function. If you want to assign it, please implement <b>set" .. k:gsub("^%l", string.upper) .. "()</b> function.")
+                    end
+                else
+                    t[k] = v
                 end
             end
         }
